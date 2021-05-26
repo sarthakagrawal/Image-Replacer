d3a57407de3fd6bf0d5ad46ee40212cae60acb80
Sarthak Agrawal <seemanjeev@gmail.com>
2021-05-26 14:01:07 -0400

    Hides name in URL

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,4 +1,6 @@
 document.documentElement.style.display = 'none';
 setTimeout(function() {document.documentElement.style.display = '';}, 1000);
+window.history.pushState("object or string", "Name Hidden", "/Name_Hidden");
 document.title = 'Name Hidden';
+
 // setTimeout(function() {document.title = 'Name Hidden';}, 1000);
